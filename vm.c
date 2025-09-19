f1745a73f8b927399b639a9f9cedac9c08b8fc5d
fuzz88 <ivan@oschepkov.ru>
2025-09-19 20:00:07 +0800

    add set globals

--- vm.c
+++ vm.c
@@ -136,6 +136,15 @@ static InterpretResult run()
                 push(value);
                 break;
             }
+            case OP_SET_GLOBAL: {
+                ObjString* name = READ_STRING();
+                if (tableSet(&vm.globals, name, peek(0))) {
+                    tableDelete(&vm.globals, name);
+                    runtimeError("Undefined variable '%s'.", name->chars);
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                break;
+            }
             case OP_POP:    pop(); break;
             case OP_NIL:    push(NIL_VAL); break;
             case OP_TRUE:   push(BOOL_VAL(true)); break;
